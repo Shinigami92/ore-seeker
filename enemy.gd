121a97ef66e60604e86f94f40a1757d7602dc3ab
Shinigami <chrissi92@hotmail.de>
2023-10-21 14:10:13 +0200

    Lint and check formatting via CI (#5)

--- enemy.gd
+++ enemy.gd
@@ -2,25 +2,29 @@ extends CharacterBody2D
 
 @export var movement_speed: float = 200.0
 
+var start_navigating: bool = false
+
 @onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
 @onready var player: CharacterBody2D = %Player
 
-var start_navigating: bool = false
 
 func _ready():
 	navigation_agent.path_desired_distance = 4.0
 	navigation_agent.target_desired_distance = 4.0
-	
+
 	call_deferred("actor_setup")
+
 
 func actor_setup():
 	await get_tree().physics_frame
-	
+
 	start_navigating = true
 
-func _physics_process(delta):
-	if !start_navigating: return
-	
+
+func _physics_process(_delta):
+	if !start_navigating:
+		return
+
 	var current_agent_position: Vector2 = global_position
 	navigation_agent.target_position = player.position
 	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
--- player.gd
+++ player.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
-@export var speed = 300
+@export var speed: float = 300.0
+
 
 func get_input():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction * speed
 
-func _physics_process(delta):
+
+func _physics_process(_delta):
 	get_input()
 	move_and_slide()
