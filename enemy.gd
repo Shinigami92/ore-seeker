26d4165cd8c48f774e7db9d2402190278b935b2b
Shinigami92 <chrissi92@hotmail.de>
2023-10-29 11:07:34 +0100

    Animated enemies

--- enemy.gd
+++ enemy.gd
@@ -1,3 +1,4 @@
+class_name Enemy
 extends CharacterBody2D
 
 @export var movement_speed: float = 150.0
@@ -25,10 +26,15 @@ func _physics_process(_delta):
 	if not start_navigating:
 		return
 
+	# target position
 	var current_agent_position: Vector2 = global_position
 	navigation_agent.target_position = player.position
 	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
 
+	# look at
+	$AnimatedSprite2D.flip_h = global_position.x < player.global_position.x
+
+	# velocity
 	var new_velocity: Vector2 = next_path_position - current_agent_position
 	new_velocity = new_velocity.normalized()
 	new_velocity = new_velocity * movement_speed
