9c8c1ccc86f2fc5ebe6386d4db880482c8bd359a
Shinigami <chrissi92@hotmail.de>
2023-10-30 17:30:08 +0100

    Mine resources (#16)

--- miner.gd
+++ miner.gd
@@ -1,10 +1,22 @@
 class_name Miner
 extends Area2D
 
+var placed_on: Ore
+var mined_amount: int = 0
+
+@onready var mining_interval_timer: Timer = $MiningIntervalTimer
+@onready var progress: ProgressBar = $MiningProgressBar
+
+
+func init(placed_on: Ore):
+	self.placed_on = placed_on
+
 
 func _ready():
-	pass
+	mining_interval_timer.timeout.connect(_on_mining_interval_timer)
 
 
-func _physics_process(_delta):
-	pass
+func _on_mining_interval_timer():
+	if not placed_on.is_depleted:
+		mined_amount += placed_on.gain_resource()
+		progress.value = mined_amount
--- ore.gd
+++ ore.gd
@@ -2,6 +2,9 @@ class_name Ore
 extends StaticBody2D
 
 @export var miner_scene: PackedScene
+@export var amount_of_resources: int = 100
+
+var is_depleted: bool = false
 
 var _miner: Miner = null
 var _player_is_near: bool = false
@@ -10,7 +13,22 @@ var _player_is_near: bool = false
 func _physics_process(_delta):
 	if _player_is_near and not _miner and Input.is_action_just_pressed("place_miner"):
 		_miner = miner_scene.instantiate()
+		_miner.init(self)
 		add_child(_miner)
+
+
+func gain_resource(take: int = 1) -> int:
+	if not is_depleted:
+		amount_of_resources -= take
+
+		if amount_of_resources <= 0:
+			is_depleted = true
+			take += amount_of_resources
+			amount_of_resources = 0
+
+		return take
+
+	return 0
 
 
 func _on_player_nearby_detector_body_entered(_body):
