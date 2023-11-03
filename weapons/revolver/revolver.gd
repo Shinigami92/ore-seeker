1439476407a8e2d2f14cfafcee82c7689ec6d297
Shinigami92 <chrissi92@hotmail.de>
2023-11-03 23:34:13 +0100

    Add reloading

--- weapons/revolver/revolver.gd
+++ weapons/revolver/revolver.gd
@@ -3,12 +3,16 @@ extends Node2D
 
 const BULLET_SCENE: PackedScene = preload("res://weapons/bullet.tscn")
 
+@export var max_ammo: int = 6
+
+var ammo: int = max_ammo
+
 @onready var shoot_interval_timer: Timer = $ShootIntervalTimer
 @onready var reload_interval_timer: Timer = $ReloadIntervalTimer
 
 
 func shoot():
-	if not shoot_interval_timer.is_stopped():
+	if ammo < 0 or not reload_interval_timer.is_stopped() or not shoot_interval_timer.is_stopped():
 		return
 
 	shoot_interval_timer.start()
@@ -17,3 +21,19 @@ func shoot():
 	get_tree().root.add_child(bullet)
 	bullet.global_position = global_position
 	bullet.look_at(get_global_mouse_position())
+	print("Bullet shot!")
+
+	ammo -= 1
+
+	if ammo <= 0:
+		reload()
+
+
+func reload():
+	if not reload_interval_timer.is_stopped():
+		return
+
+	reload_interval_timer.start()
+	print("Reload")
+
+	ammo = max_ammo
