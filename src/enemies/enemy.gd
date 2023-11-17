a478a7d13be906f5a68f4e8225b4753e9389f679
Shinigami92 <chrissi92@hotmail.de>
2023-11-17 17:06:35 +0100

    Cleanup on player entered hub

--- src/enemies/enemy.gd
+++ src/enemies/enemy.gd
@@ -12,12 +12,22 @@ var stuck_counter: int = 0
 
 var health: int = MAX_HEALTH
 
+var _player_hub: PlayerHub
+
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var stuck_timer: Timer = $StuckTimer
 
 
+static func instantiate(player_hub: PlayerHub) -> Enemy:
+	var enemy: Enemy = preload("res://src/enemies/enemy.tscn").instantiate()
+	enemy._player_hub = player_hub
+	return enemy
+
+
 func _ready() -> void:
 	stuck_timer.timeout.connect(_on_stuck_timer_timeout)
+
+	_player_hub.player_entered.connect(_on_player_hub_player_entered)
 
 
 func _process(_delta: float) -> void:
@@ -53,6 +63,11 @@ func _on_stuck_timer_timeout() -> void:
 		stuck_counter = 0
 
 	last_position = global_position
+
+
+func _on_player_hub_player_entered() -> void:
+	# Despawn the enemy when the player enters the player hub
+	queue_free()
 
 
 func take_damage(damage: int) -> void:
