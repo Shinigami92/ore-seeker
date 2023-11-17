6ccaf944a0843520045ffc7542cedc2e458f93d1
Shinigami92 <chrissi92@hotmail.de>
2023-11-17 19:34:34 +0100

    Rebuild chunk tilemap

--- src/world/chunks/chunk.gd
+++ src/world/chunks/chunk.gd
@@ -18,4 +18,27 @@ static func instantiate(coordinated: Vector2i) -> Chunk:
 func _ready() -> void:
 	visibility_notifier.screen_entered.connect(tile_map.show)
 	visibility_notifier.screen_exited.connect(tile_map.hide)
+
+	# Stones
+	for i in range(4):
+		var atlas_coords: Vector2i = Vector2i(randi() % 8, randi() % 3)
+		_place_random_cell(atlas_coords, 0)
+
+	# Grass
+	for i in range(6):
+		var atlas_coords: Vector2i = Vector2i(randi() % 15, randi() % 6)
+		_place_random_cell(atlas_coords, 1)
+
+	# Flowers
+	for i in range(2):
+		var atlas_coords: Vector2i = Vector2i(randi() % 6, 6)
+		_place_random_cell(atlas_coords, 1)
+
 	tile_map.visible = false
+
+
+func _place_random_cell(atlas_coords: Vector2i, source_id: int) -> void:
+	var x: int = randi() % CHUNK_SIZE
+	var y: int = randi() % CHUNK_SIZE
+	var coords: Vector2i = Vector2i(x, y)
+	tile_map.set_cell(0, coords, source_id, atlas_coords, 0)
