32dedf120cdb6b2c0ffdda13524009bffa777f79
Shinigami92 <chrissi92@hotmail.de>
2023-11-18 20:50:35 +0100

    Regenerate chunks with ores

--- src/world/chunks/chunk.gd
+++ src/world/chunks/chunk.gd
@@ -5,19 +5,28 @@ const CHUNK_SIZE = 16
 const TILE_SIZE = 64
 const CHUNK_WIDTH = CHUNK_SIZE * TILE_SIZE
 
+var _coordinated: Vector2i
+var _noise: Noise
+
+var _player_hub: PlayerHub
+
 @onready var tile_map: TileMap = $TileMap
 @onready var visibility_notifier: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
 
 
-static func instantiate(coordinated: Vector2i) -> Chunk:
+static func instantiate(coordinated: Vector2i, noise: Noise, player_hub: PlayerHub) -> Chunk:
 	var chunk: Chunk = preload("res://src/world/chunks/chunk.tscn").instantiate()
-	chunk.position = coordinated * CHUNK_WIDTH
+	chunk._coordinated = coordinated
+	chunk._noise = noise
+	chunk._player_hub = player_hub
 	return chunk
 
 
 func _ready() -> void:
 	visibility_notifier.screen_entered.connect(tile_map.show)
 	visibility_notifier.screen_exited.connect(tile_map.hide)
+
+	position = _coordinated * CHUNK_WIDTH
 
 	# Stones
 	for i in range(4):
@@ -33,6 +42,14 @@ func _ready() -> void:
 	for i in range(2):
 		var atlas_coords: Vector2i = Vector2i(randi() % 6, 6)
 		_place_random_cell(atlas_coords, 1)
+
+	var noise_value: float = _noise.get_noise_2dv(_coordinated)
+
+	# TODO christopher 2023-11-18: Check for better probability distribution
+	if noise_value * _coordinated.length() > 0.8:
+		var ore: Ore = Ore.instantiate(_player_hub)
+		ore.position = Vector2i(randi_range(0, CHUNK_SIZE), randi_range(0, CHUNK_SIZE)) * TILE_SIZE
+		add_child(ore)
 
 	tile_map.visible = false
 
--- src/world/chunks/outer_world_chunk_gen.gd
+++ src/world/chunks/outer_world_chunk_gen.gd
@@ -2,11 +2,23 @@ class_name OuterWorldChunkGen
 extends Marker2D
 
 @export var player: Player
+@export var player_hub: PlayerHub
 
+var _generation_active: bool = false
 var _chunks: Dictionary = {}
+var _noise: FastNoiseLite = FastNoiseLite.new()
+
+
+func _ready() -> void:
+	_noise.noise_type = FastNoiseLite.TYPE_SIMPLEX
+	_noise.fractal_octaves = 4
+	_noise.frequency = 1.0 / 20.0
 
 
 func _process(_delta: float) -> void:
+	if not _generation_active:
+		return
+
 	var player_chunk: Vector2i = Vector2i(floor(player.global_position / Chunk.CHUNK_WIDTH))
 
 	for dx in [-1, 0, 1]:
@@ -14,13 +26,24 @@ func _process(_delta: float) -> void:
 			var chunk_coordinates: Vector2i = player_chunk + Vector2i(dx, dy)
 
 			if not _chunks.has(chunk_coordinates):
-				var chunk: Chunk = Chunk.instantiate(chunk_coordinates)
+				var chunk: Chunk = Chunk.instantiate(chunk_coordinates, _noise, player_hub)
 				add_child(chunk)
 				_chunks[chunk_coordinates] = chunk
 
-	for chunk_coordinates in _chunks:
-		if (chunk_coordinates - player_chunk).length() >= 3:
-			var chunk: Chunk = _chunks.get(chunk_coordinates)
 
-			if _chunks.erase(chunk_coordinates):
-				chunk.queue_free()
+func _on_player_hub_player_exited() -> void:
+	randomize()
+
+	_noise.seed = randi()
+	print("Seed: %d" % _noise.seed)
+
+	_generation_active = true
+
+
+func _on_player_hub_player_entered() -> void:
+	_generation_active = false
+
+	for chunk in _chunks.values():
+		chunk.queue_free()
+
+	_chunks.clear()
