c9c4f939cb2a7d5e9ef5def4ae1291ff3b01cd9b
Shinigami92 <chrissi92@hotmail.de>
2023-11-20 16:19:45 +0100

    Show coordinates on hud

--- src/hud/hud.gd
+++ src/hud/hud.gd
@@ -4,10 +4,16 @@ extends Camera2D
 @export var player: Player
 
 @onready var player_carried_dirithium_label: Label = $PlayerCarriedDirithium
+@onready var player_corrdinates_label: Label = $PlayerCoordinates
 
 
 func _ready() -> void:
 	player.carried_dirithium_changed.connect(_redraw_player_carried_dirithium_label)
+
+
+func _process(_delta: float) -> void:
+	var player_position: Vector2i = player.position * Vector2(1, -1) / Chunk.TILE_SIZE
+	player_corrdinates_label.text = "%s" % [player_position]
 
 
 func _redraw_player_carried_dirithium_label(carried_dirithium: int) -> void:
